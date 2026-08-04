db955ae71902b73e9b95cbbe3f0ffe1107fd5589
sarthak5577 <sarthakvarpe022@gmail.com>
2026-08-04 09:56:05 +0530

    add

--- DSA/assignment_2..py
+++ DSA/assignment_2..py
@@ -1,7 +1,7 @@
-count=int(input("Enter how many workers id's you want to insert:- "))
+count=int(input("Enter how many Customer id's you want to insert:- "))
 a=[]
 for i in range(count):
-    temp=int(input(f"Enter the ID's of Worker {i+1}:- "))
+    temp=int(input(f"Enter the ID's of Customer {i+1}:- "))
     a.append(temp)
 number= sorted(a)
 
@@ -13,7 +13,7 @@ target=int(input("Enter the ID you want to search:- "))
 while start<=end:
     mid=(start+end)//2
     if number[mid]==target:
-        print("Element found at index:- ",mid)
+        print("Customer IDfound at index:- ",mid)
         found=True
         break
     elif number[mid]<target:
@@ -21,5 +21,5 @@ while start<=end:
     else:
         end=mid-1
 if found==False:
-    print("Element not found")
+    print("Customer ID not found")
 
