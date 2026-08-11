5b50720b1023eb439595a9b4853a58898171d83c
sarthak5577 <sarthakvarpe022@gmail.com>
2026-08-11 09:35:59 +0530

    adding bubble sorting

---
+++ DSA/assignment_3.py
@@ -0,0 +1,19 @@
+#bubble sorting
+def sort(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+
+no=int(input("Enter the number of elements in the array: "))
+print("Enter unsorted array elements:-")
+arr=[]
+for i in range(no):
+    ele=int(input(f"Enter the element {i+1}:- ")) 
+    arr.append(ele)
+
+sort(arr)
+print("Array after sorting:", arr)
+
+#selection sorting
