3a02af457dd9718dcbf9eac17a6465a85f641b73
sarthak5577 <sarthakvarpe022@gmail.com>
2026-08-12 09:34:58 +0530

    ADDING SELECTION SORTING

--- DSA/assignment_3.py
+++ DSA/assignment_3.py
@@ -1,19 +1,37 @@
 #bubble sorting
-def sort(arr):
+# def sort(arr):
+#     n=len(arr)
+#     for i in range(n):
+#         for j in range(0,n-i-1):
+#             if arr[j]>arr[j+1]:
+#                 arr[j],arr[j+1]=arr[j+1],arr[j]
+
+# no=int(input("Enter the number of elements in the array: "))
+# print("Enter unsorted array elements:-")
+# arr=[]
+# for i in range(no):
+#     ele=int(input(f"Enter the element {i+1}:- ")) 
+#     arr.append(ele)
+
+# sort(arr)
+# print("Array after sorting:", arr)
+
+#selection sorting 
+def selection_sort(arr):
     n=len(arr)
     for i in range(n):
-        for j in range(0,n-i-1):
-            if arr[j]>arr[j+1]:
-                arr[j],arr[j+1]=arr[j+1],arr[j]
-
-no=int(input("Enter the number of elements in the array: "))
-print("Enter unsorted array elements:-")
+        si=i
+        for j in range(i+1,n):
+            if arr[j]<arr[si]:
+                si=j
+        arr[i],arr[si]=arr[si],arr[i]
+        
+n=int(input("Enter the number of elements in the array: "))
+print("--Enter unsorted array elements--")
 arr=[]
-for i in range(no):
+for i in range(n):
     ele=int(input(f"Enter the element {i+1}:- ")) 
     arr.append(ele)
 
-sort(arr)
-print("Array after sorting:", arr)
-
-#selection sorting
+selection_sort(arr)
+print("Array after selection sorting:", arr)
